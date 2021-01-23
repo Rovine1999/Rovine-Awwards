4cf2807755a2a6db958235d5d532c82a90691f91
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-24 00:55:27 +0300

    add view function for user registration

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        profileForm=ProfileForm(request.POST, request.FILES)
+        if form.is_valid() and profileForm.is_valid():
+            username=form.cleaned_data.get('username')
+            user=form.save()
+            profile=profileForm.save(commit=False)
+            profile.user=user
+            profile.save()
+            messages.success(request, f'Successfully created Account!.You can now login as {username}!')
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+        profileForm=ProfileForm()
+    context={
+        'form':form,
+        'profileForm': profileForm
+    }
+    return render(request, 'registration/registration.html', context)
