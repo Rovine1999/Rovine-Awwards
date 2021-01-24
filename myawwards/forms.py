671a17a26c3931aa3b74cf92eb19375b4d33ace7
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-24 19:57:14 +0300

    add project form for user to post project

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Image,Profile,Comment
+from .models import Project
 from django_registration.forms import RegistrationForm
 
     
-class NewProjectForm(forms.ModelForm):
+class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['pub_date', 'Developer', 'developer_profile']
@@ -11,20 +11,20 @@ class NewProjectForm(forms.ModelForm):
           'project_description': forms.Textarea(attrs={'rows':4, 'cols':10,}),
         }
         
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ['user']
-        widgets = {
-          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
-        }]
-    class Meta:
-        model = Comment
-        exclude = ['pub_date', 'developer']
+# class ProfileUpdateForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude = ['user']
+#         widgets = {
+#           'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+#         }
+#     class Meta:
+#         model = Comment
+#         exclude = ['pub_date', 'developer']
         
-class NewsLetterForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
+# class NewsLetterForm(forms.Form):
+#     your_name = forms.CharField(label='First Name',max_length=30)
+#     email = forms.EmailField(label='Email')
         
         
         
