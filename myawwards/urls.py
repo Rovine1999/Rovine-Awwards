44ac6973eb36c085b64ea9ce0911bbdf2db63ef5
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-24 22:07:45 +0300

    add url for login view function

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -4,8 +4,9 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [ 
- re_path('register/',views.register, name='registration'),
+    re_path('register/',views.register, name='registration'),
     path('profile/',views.profile, name='profile'),
+    re_path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
