c33c62135335918fc0117a6096df2a9b3b9247ed
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-24 19:55:54 +0300

    add user project models

--- myawwards/models.py
+++ myawwards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField('image')
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+        
+    
+    
