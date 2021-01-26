1a4d173a5054b820288e37184f84a640a698b231
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-26 23:03:30 +0300

    add url redirect for home page

--- Awward/settings.py
+++ Awward/settings.py
@@ -136,4 +136,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGIN_URL = 'login'
