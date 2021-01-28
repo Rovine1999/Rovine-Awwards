a364b412bf6d158a6a8450a8cbaaf0df5fb8ae62
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-28 11:40:15 +0300

    import project and profile from the models

--- myawwards/views.py
+++ myawwards/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from django.contrib.auth.decorators import login_required
-from .models import Project
-from .forms import ProjectForm,RegistrationForm
+from .models import Project,Profile
+from .forms import ProjectForm,RegistrationForm,ProfileUpdateForm
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView,DeleteView)
 
 # Create your views here.
+
+
+@login_required
+def awwards(request):
+    images = Project.objects.all()
+   
+        
+    return render(request, 'awwards.html',{'images':images})
+
 def register(request):
     if request.method=="POST":
         form=RegistrationForm(request.POST)
@@ -17,7 +27,7 @@ def register(request):
             profile.user=user
             profile.save()
             messages.success(request, f'Successfully created Account!.You can now login as {username}!')
-        return redirect('login')
+            return redirect('login')
     else:
         form= RegistrationForm()
         profileForm=ProjectForm()
@@ -25,25 +35,65 @@ def register(request):
         'form':form,
         'profileForm': profileForm
     }
-    return render(request, 'django_registration/registration_form.html', context)
+    return render(request, 'registration/registration.html', context)
 
 
 
-@login_required
-def profile(request):
+def awwards(request):
+    current_user = request.user
     if request.method == 'POST':
-        useForm=UserUpdateForm(request.POST, instance=request.user)
-        profileForm=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if useForm.is_valid() and profileForm.is_valid():
-            useForm.save()
-            profileForm.save()
-            messages.success(request, f'Your account has been updated!')
-        return redirect('profile')
+        form =ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('home')
+
     else:
-        useForm=UserUpdateForm(instance=request.user.profile)
-        profileForm=ProfileUpdateForm(instance=request.user.profile)
-    context={
-        'useForm':useForm,
-        'profileForm':profileForm
-    }
-    return render(request, 'registration/profile.html', context)
+        form =ProjectForm()
+    return render(request, 'awwards.html', {"form":form})
+
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    developer = current_user
+    project = Project.get_by_developer(developer)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.photo)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+        
+    else:
+        form = ProfileUpdateForm()    
+    return render(request, 'django_registration/profile.html', {"form":form, "project":project})
+
+
+@login_required(login_url='/accounts/login/')
+def search_awward(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_awward = Project.search_awward(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"project": searched_awward})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
+
+
+
+
+
+class PostListView(ListView):
+    model = Project
+    template_name = 'index.html'
+    context_object_name = 'posts'
+    ordering = ['-pub_date']
